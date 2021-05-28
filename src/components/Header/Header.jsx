519e0c9548571ebe77f5fc747cd3ff938ae22818
Ivan Nemirich <bebded@gmail.com>
2021-05-29 01:12:05 +0700

    fix: header logo link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import './Header.css';
 function Header() {
   return(
     <header className='header'>
-      <img className='header__logo' src={logo} alt='Диплом Лого' />
+      <a className='header__logo' href='/'>
+        <img src={logo} alt='Диплом Лого' />
+      </a>
       <div className='header__container'>
         <a className='header__singup-btn header-btn' href='/signup'>Регистрация</a>
         <a className='header__login-btn header-btn' href='/signin'>Войти</a>
