2e494aec384a0e359d040bd6ed71e7bcc9ba0184
Ivan Nemirich <bebded@gmail.com>
2021-06-05 19:24:36 +0700

    add: валидация в profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,6 +11,10 @@ function Profile(props) {
 
   const [email, setEmail] = useState();
   const [name, setName] = useState();
+  const [emailError, setEmailError] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailValid, setEmailValid] = useState('false');
+  const [nameValid, setNameValid] = useState('false');
 
   useEffect(() => {
     setEmail(currentUser.email);
@@ -19,9 +23,32 @@ function Profile(props) {
 
   function handleChangeEmail(e) {
     setEmail(e.target.value)
+
+    const val = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (!e.target.value) {
+      setEmailError('Email не может быть пустым');
+      setEmailValid(false);
+    } else if (!val.test(String(e.target.value).toLowerCase())) {
+      setEmailError('Некорректный email');
+      setEmailValid(false);
+    } else {
+      setEmailError('');
+      setEmailValid(true);
+    }
   }
   function handleChangeName(e) {
     setName(e.target.value)
+
+    if(!e.target.value) {
+      setNameError('Имя не может быть пустым')
+      setNameValid(false);
+    } else if (e.target.value.length <= 2) {
+      setNameError('Имя должно быть больше 2-х символов')
+      setNameValid(false);
+    } else {
+      setNameError('')
+      setNameValid(true);
+    }
   }
 
   function handleSubmit(e) {
@@ -49,7 +76,7 @@ function Profile(props) {
               <input onChange={handleChangeName} value={name} className='profile__value profile__value-name' type='name'
                      name='email'
                      required='' minLength='2' maxLength='30' />
-              <span className='form__error profile__error'>Где Имя Лебовский?</span>
+              <span className='form__error profile__error'>{nameError}</span>
 
             </div>
             <div className='profile__input'>
@@ -58,13 +85,18 @@ function Profile(props) {
               <input onChange={handleChangeEmail} value={email} className='profile__value profile__value-email' type='email'
                      name='email'
                      required='' minLength='2' maxLength='30' />
-              <span className='form__error profile__error'>Где email Лебовский?</span>
+              <span className='form__error profile__error'>{emailError}</span>
 
             </div>
           </div>
         </div>
         <div className='profile__buttons'>
-          <button onClick={handleSubmit} className='profile__btn profile__edit-btn' type='submit'>Редактировать</button>
+          <button onClick={handleSubmit}  type='submit'
+                  className={ `profile__btn profile__edit-btn ${emailValid && nameValid ? '' : '_unactive'}`}
+                  disabled={emailValid && nameValid ? false : true}
+          >
+            Редактировать
+          </button>
           <button onClick={props.handleSignOut} className='profile__btn profile__exit-btn' type='button'>Выйти из аккаунта</button>
         </div>
 
